26c0bd50202673ae7a0afd02153d047170340c57
chrizxzayax <chmolina629@hotmail.com>
2025-10-02 22:49:46 -0700

    add the inputting question

--- lab18.cpp
+++ lab18.cpp
@@ -8,14 +8,22 @@ struct ReviewNode
 {
     double rating;
     string comment;
+
+    ReviewNode* next;
 };
 
 void addHead(ReviewNode*& head, double rating, const string& comment);
 
 int main()
 {   
+    ReviewNode* head = nullptr;
+    int mode;
     
+    cout << "which of the linked list method should we use? (1 for head, 2 for tail): ";
+    cin >> mode;
+    cin.ignore(); // to clear the newline character from the input buffer
 
-
+    bool moreR = true;
+    
 
 }
