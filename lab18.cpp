38407d71a26eb8d174cbe88ea0787e35be04cdf5
chrizxzayax <chmolina629@hotmail.com>
2025-10-02 22:37:05 -0700

    Initial commit

---
+++ lab18.cpp
@@ -0,0 +1,21 @@
+// comsc 210 | lab18 | Christian Molina
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct ReviewNode
+{
+    double rating;
+    string comment;
+};
+
+void addHead(ReviewNode*& head, double rating, const string& comment);
+
+int main()
+{   
+    
+
+
+
+}
